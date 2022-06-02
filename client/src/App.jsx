ae09c69fd98f59f11304efdab7e5c17f0d41bd49
mehdiEnnabli <mehdi.ennabli01@gmail.com>
2022-06-02 18:02:54 +0100

    form

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "./components/Form/Form.jsx";
+function App() {
+  return (
+    <div style={{backgroundImage:'client\src\images\snes.webp'  }}>
+      Hello
+      <div><Form /></div>
+    </div>
+  );
+}
+
+export default App;
